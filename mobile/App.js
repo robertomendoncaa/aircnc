3cfb9f80129eda6ba126a269dc25de4563d988be
Roberto <robertomendonca20@gmail.com>
2019-10-04 16:34:27 -0300

    Initial commit Mobile Expo

---
+++ mobile/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={{ fontSize: 42, fontWeight: 'bold', color: '#333' }}>Aircnc</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
