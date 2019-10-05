a77abf8fbbf5991461e51581fefad84baa4c5fc7
Roberto <robertomendonca20@gmail.com>
2019-10-05 11:58:23 -0300

    fix config

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-  DB_URL='mongodb+srv://admin:admin@cluster0-zud0n.mongodb.net/aircnc?retryWrites=true&w=majority'
+  DB_URL: 'mongodb+srv://admin:admin@cluster0-zud0n.mongodb.net/aircnc?retryWrites=true&w=majority'
 }
